096f4711e20d7b832cc03009bc6a6eb14dadae95
Luis Jimenez <lajp0097@gmail.com>
2023-12-26 22:43:44 -0600

    disign tournaments view

--- app-quinielas-layfer/src/app/app-routing.module.ts
+++ app-quinielas-layfer/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TournamentsComponent } from './tournaments/tournaments.component';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'torneos', component: TournamentsComponent},
+  {path: '**' , component: NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- app-quinielas-layfer/src/app/app.module.ts
+++ app-quinielas-layfer/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { TournamentsComponent } from './tournaments/tournaments.component';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    TournamentsComponent,
+    HomeComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
